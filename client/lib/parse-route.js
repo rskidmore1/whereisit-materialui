4c9173a3b27a1343112d1cc2b07b9f18b3ccc11e
rskidmore1 <strictlybusinessbythebook@yahoo.com>
2021-10-08 00:21:27 +0000

    Add material UI kit code

--- client/lib/parse-route.js
+++
@@ -1,8 +0,0 @@
-export default function parseRoute(hashRoute) {
-  if (hashRoute.startsWith('#')) {
-    hashRoute = hashRoute.replace('#', '');
-  }
-  const [path, queryString] = hashRoute.split('?');
-  const params = new URLSearchParams(queryString);
-  return { path, params };
-}
